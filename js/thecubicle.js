3768d7038f1ff834804024f0b82339ea8aa770d9
westonmizumoto <westonm1@stanford.edu>
2015-09-01 01:40:15 -0700

    changed protocol

--- js/thecubicle.js
+++ js/thecubicle.js
@@ -3,7 +3,7 @@
 function getCubicleResultsFor(query){
 	//query should be a space separated string of keywords
 	//Returns an object with title:price
-	var url = "https://thecubicle.us/advanced_search_result.php?search_in_description=0&keywords=";
+	var url = "//thecubicle.us/advanced_search_result.php?search_in_description=0&keywords=";
 	var delimiter = "+";
 	var terms = query.split(" ");
 	for (var term in terms){
